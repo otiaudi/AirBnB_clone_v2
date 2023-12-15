8544838e50d67b1a92107ad5b99a1084c35b8672
otiaudi <otiaudi9@gmail.com>
2023-12-15 16:36:55 +0300

    updated the file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-#  script that sets up web servers for the deployment of web_static
-
-sudo apt-get update -y
-sudo apt-get install nginx -y
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-echo "<html><head></head><body>This is silas test page</body></html>"|sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}'/etc/nginx/sites-available/default
-sudo service nginx restart
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "ALX program is real" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
 exit 0
-
